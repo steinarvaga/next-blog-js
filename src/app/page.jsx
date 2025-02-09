12a370a125a7482569b67877878a39576ee10b99
steinarvaga <steinarv.react.utvikler@gmail.com>
2025-02-09 02:12:42 +0100

    Install Next.js and Tailwind CSS and create the first template

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-red-500 text-5xl">Home</div>
+    </div>
+  );
+}
